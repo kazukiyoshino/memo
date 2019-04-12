e5da72dd0df3e9e8625838eb1c49d167d0a17fbd
kazukiyoshino <yoshino@genuine-pt.jp>
2019-04-12 14:52:55 +0900

    変数名の修正

--- console/src/console/Kadai3.java
+++ console/src/console/Kadai3.java
@@ -29,21 +29,22 @@ public class Kadai3 {
 	}
 
 
-	public static boolean validation(String Check[]) {
+	public static boolean validation(String check[]) {
 
-		if(isArgCheck(Check)&&isNumCheck(Check[0],Check[1])&&isSymCheck(Check[2])) {
+		if(isArgCheck(check)&&isNumCheck(check[0],check[1])&&isSymCheck(check[2])) {
 			return true;
 		}
 		return false;
 	}
 
 
-	public static boolean isArgCheck(String CheckBound[]) {
-		if(CheckBound.length<3){
+
+	public static boolean isArgCheck(String checkBound[]) {
+		if(checkBound.length<3){
 			System.out.println("引数が足りません");
 			return false;
 		}
-		if(CheckBound.length>3) {
+		if(checkBound.length>3) {
 			System.out.println("引数が多すぎます");
 			//System.out.println(CheckBound[3]);
 			//「*」のみコマンドライン引数に出来ず。上記では「.project」が出現(ワイルドカード扱い？)
@@ -54,12 +55,12 @@ public class Kadai3 {
 	}
 
 
-	public static boolean isNumCheck(String CheckNum1,String CheckNum2) {
+	public static boolean isNumCheck(String checkNum1,String checkNum2) {
 		int i=1;
 		try {
-			Double.parseDouble(CheckNum1);
+			Double.parseDouble(checkNum1);
 			i++;
-			Double.parseDouble(CheckNum2);
+			Double.parseDouble(checkNum2);
 			return true;
 		}catch(NumberFormatException e){
 			System.out.println(i+"番目の引数は数字で入力してください");
@@ -68,8 +69,8 @@ public class Kadai3 {
 	}
 
 
-	public static boolean isSymCheck(String CheckStr) {
-		switch(CheckStr) {
+	public static boolean isSymCheck(String checkSym) {
+		switch(checkSym) {
 			case"+":
 			case"-":
 			case"*":
