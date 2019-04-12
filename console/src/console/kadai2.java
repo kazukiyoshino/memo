74b7d7b6be247940b7d34de26fbeec88957a368a
kazukiyoshino <yoshino@genuine-pt.jp>
2019-04-12 12:39:39 +0900

    fix & cus

---
+++ console/src/console/kadai2.java
@@ -0,0 +1,44 @@
+package console;
+
+public class kadai2 {
+
+	public static void main(String[] args) {
+		
+		// 異なる解を模索(無理矢理メソッドを分けてみる)
+		if(!isNumberCheck(args)) {
+			System.out.println("数字で入力してください");
+			System.exit(0);
+		}
+		
+		double doubleargs[] = parse(args);
+		double ans=0.0;
+		for(double i :doubleargs) {
+			ans += i;
+		}
+		System.out.println(ans);
+		
+		//DecimalFormat以外の方法は…
+
+	}
+	
+	public static boolean isNumberCheck(String checkNums[]) {
+		try {
+			for(int i=0;i<checkNums.length;i++) {
+			  Double.parseDouble(checkNums[i]);
+			}
+			 return true;
+		}catch(NumberFormatException e){
+			return false;
+		}
+	}
+		
+	public static double[] parse(String parseNums[]){
+		double arg[] =new double[parseNums.length];
+		for(int i=0;i<parseNums.length;i++) {
+			arg[i]=Double.parseDouble(parseNums[i]);
+			}
+		return arg;
+		
+	}
+
+}
